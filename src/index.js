187a63e9fea071ee8a2fa3d01aacc86c222df80e
Johann Folta <jfolta@kyruus.com>
2021-03-05 09:17:05 -0500

    resource reading endpoints: part 2

--- src/index.js
+++ src/index.js
@@ -64,6 +64,30 @@ app.post("/tasks", (req, res) => {
     });
 });
 
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  Task.findById(req.params.id)
+    .then((task) => {
+      if (!task) {
+        res.status(400).send();
+      }
+
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
