923a7b92c21ae642aabc2f132cb296623f3eb377
Johann Folta <jfolta@kyruus.com>
2021-05-04 16:34:35 -0400

    Resource Deleting Endpoints

--- src/index.js
+++ src/index.js
@@ -75,6 +75,20 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ error: "No matching user to delete!" });
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
@@ -136,6 +150,20 @@ app.patch("/tasks/:id", async (req, res) => {
     res.send(task);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send({ error: "No matching task to delete!" });
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
